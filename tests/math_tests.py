c4f0067356868313f4d124970ef7455dbf2ef740
Jaroslav Hajek <hajda14@gmail.com>
2022-07-04 17:13:06 +0200

    nupy has its own random

--- tests/math_tests.py
+++ tests/math_tests.py
@@ -1,13 +1,13 @@
 import pytest 
 import requests
 import time
-from numpy import random
+from numpy import random as nprandom
 import random
 
 def test_sum_numbers():
      
      for i in range(random.randint(1000, 3000)):
-          rand_array=random.randint(100, size=(5)).tolist()
+          rand_array=nprandom.randint(100, size=(5)).tolist()
           response = requests.get("http://10.1.0.10:5620/math/sum",json=rand_array)
           for slave in response.json():
                assert slave['result'] == sum(rand_array)
