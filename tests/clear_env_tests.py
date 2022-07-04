63767e157881350b18596db1b13d1d58de6417d9
Jaroslav Hajek <hajda14@gmail.com>
2022-07-04 19:12:04 +0200

    added  masterip from file to clear test

--- tests/clear_env_tests.py
+++ tests/clear_env_tests.py
@@ -4,5 +4,8 @@ import requests
 
 
 def test_clear_all_logs():
-     response = requests.get("http://10.1.0.10:5620/management/log/clearall")
+     MASTER_IP=""
+     with open("masterIP",'r') as f:
+          MASTER_IP=f.read().strip()
+     response = requests.get(f"http://{MASTER_IP}:5620/management/log/clearall")
      assert response.status_code == 200
