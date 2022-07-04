c63fcafe0e3d186a69e041a532aa8330ecb29cfb
Jaroslav Hajek <hajda14@gmail.com>
2022-07-04 18:50:17 +0200

    fixed typo writ->write

---
+++ app/getMasterNode.py
@@ -0,0 +1,10 @@
+import socket, sys
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            server_address = ('', 5621)
+            sock.bind(server_address)
+            while True:
+                data, address = sock.recvfrom(1024)
+                data = str(data, 'utf-8')
+                if str(data)=="INTERNSHIP-DISCOVERY":
+                    sock.close()
+                    sys.stdout.write(address[0])
