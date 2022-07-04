8dcf12f8ee185b5f1e036f6e93d9b8c7804d2848
Jaroslav Hajek <hajda14@gmail.com>
2022-07-04 19:07:08 +0200

    socket should be closed but not in while true

--- app/getMasterNode.py
+++ app/getMasterNode.py
@@ -2,8 +2,8 @@ import socket, sys
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             server_address = ('', 5621)
             sock.bind(server_address)
-            while True:
-                data, address = sock.recvfrom(1024)
-                data = str(data, 'utf-8')
-                if str(data)=="INTERNSHIP-DISCOVERY":
-                    sys.stdout.write(address[0])
+            data, address = sock.recvfrom(1024)
+            data = str(data, 'utf-8')
+            if str(data)=="INTERNSHIP-DISCOVERY":
+                sock.close()
+                sys.stdout.write(address[0])
